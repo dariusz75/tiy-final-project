5ded4e5129c707b9dd8e5454a3a6707cb9e8f33e
Dariusz Franczak <darius32@op.pl>
2016-04-29 16:11:36 +0100

    Some imput fields added to registration form

--- frontend/build/js/registration-details-developer.js
+++ frontend/build/js/registration-details-developer.js
@@ -2,33 +2,117 @@ var React = require('react');
 var Button = require('./button');
 
 var RegistrationDetailsDeveloper = React.createClass({
-	displayName: 'RegistrationDetailsDeveloper',
+							displayName: 'RegistrationDetailsDeveloper',
 
-	render: function () {
-		return React.createElement(
-			'div',
-			{ className: 'row img-rounded registration-developer-wrapper' },
-			React.createElement(
-				'div',
-				{ className: 'row' },
-				React.createElement(
-					'div',
-					{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
-					React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'PERSONAL DETAILS' })
-				),
-				React.createElement(
-					'div',
-					{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
-					React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'SKILLS AND EXPERIENCE' })
-				),
-				React.createElement(
-					'div',
-					{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
-					React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'ABOUT ME' })
-				)
-			)
-		);
-	}
+							render: function () {
+														return React.createElement(
+																					'div',
+																					{ className: 'row img-rounded registration-developer-wrapper' },
+																					React.createElement(
+																												'div',
+																												{ className: 'row' },
+																												React.createElement(
+																																			'div',
+																																			{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
+																																			React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'PERSONAL DETAILS' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
+																																			React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'SKILLS AND EXPERIENCE' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4' },
+																																			React.createElement(Button, { addClass: 'blue-bright fullwidth no-border button-text-style hvr-pulse-grow', text: 'ABOUT ME' })
+																												)
+																					),
+																					React.createElement(
+																												'div',
+																												{ className: 'row' },
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'First name:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Confirm email address:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Surname:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Confirm email address:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Job title:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-offset-6 col-sm-offset-6 col-sm-offset-6 col-sm-offset-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'   '
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Enter email address:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												),
+																												React.createElement(
+																																			'div',
+																																			{ className: 'form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6' },
+																																			React.createElement(
+																																										'label',
+																																										{ 'for': 'usr' },
+																																										'Confirm email address:'
+																																			),
+																																			React.createElement('input', { type: 'text', className: 'form-control', id: 'usr' })
+																												)
+																					)
+														);
+							}
 });
 
 module.exports = RegistrationDetailsDeveloper;
--- frontend/source/js/registration-details-developer.jsx
+++ frontend/source/js/registration-details-developer.jsx
@@ -14,6 +14,40 @@ var RegistrationDetailsDeveloper = React.createClass({
 	  						<div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xlg-4">
 	  							<Button addClass="blue-bright fullwidth no-border button-text-style hvr-pulse-grow" text="ABOUT ME" />
 	  						</div>
+							</div>
+							<div className="row">
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">First name:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Confirm email address:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Surname:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Confirm email address:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Job title:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-offset-6 col-sm-offset-6 col-sm-offset-6 col-sm-offset-6">
+	  							<label for="usr">   </label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Enter email address:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
+								<div className="form-group col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xlg-6">
+	  							<label for="usr">Confirm email address:</label>
+	  							<input type="text" className="form-control" id="usr"></input>
+								</div>
 							</div>				
 						 </div>;
 		}
